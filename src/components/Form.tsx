cfbdf7647d86a2c8db77dd8fc0fa94e20db41720
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2023-01-24 10:43:54 -0500

    figured out mapping problem
    On people useState, subbed 'object' to the People interface which defines the people object with the appropiate key data types

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,8 +2,14 @@ import React, { useState } from 'react'
 
 const Form: React.FC = () => {
 
+    interface People {
+        name: string,
+        age: number
+    }
+
     const [name, setName] = useState<string>("")
     const [age, setAge] = useState<number>(0)
+    const [people, setPeople] = useState<People[]>([])
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault()
@@ -11,19 +17,29 @@ const Form: React.FC = () => {
             name,
             age
         }
-        console.log(formData)
+        setPeople([...people, formData])
     }
 
   return (
     <div>
         <form onSubmit={handleSubmit}>
             <label htmlFor='name'> </label>
+            Name
             <input type='input' id='name' name='name' onChange={e => setName(e.target.value)}></input>
             <br></br>
             <label htmlFor='age'></label>
+            Age
             <input type='number' id='age' name='age' onChange={e => setAge(parseInt(e.target.value))}></input>
             <br></br>
             <button>Submit</button>
+            <div>
+              {people.map(p => (
+                <ul>
+                    <li>Name: {p.name}</li>
+                    <li>Age: {p.age}</li>
+                </ul>
+              ))}
+            </div>
         </form>
     </div>
   )
